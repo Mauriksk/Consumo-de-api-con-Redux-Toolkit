3e66e2e6c53c60c92d34c1edbf82755e65ca421a
Mauricio Torres <otarksk@gmail.com>
2022-03-28 08:00:15 -0300

    Datos de prueba extraidos con exito de redux

--- src/components/ListarUsuariosAdorno.jsx
+++ src/components/ListarUsuariosAdorno.jsx
@@ -1,29 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+//Redux
+import { fetchAllUsers } from '../store/slices/users'
+import { useDispatch } from 'react-redux'
 
 export const ListarUsuariosAdorno = () => {
 
-    const users = [
-        {
-        "id": 7,
-        "email": "michael.lawson@reqres.in",
-        "first_name": "Michael",
-        "last_name": "Lawson",
-        "avatar": "https://reqres.in/img/faces/7-image.jpg"
-        },
-        {
-        "id": 8,
-        "email": "lindsay.ferguson@reqres.in",
-        "first_name": "Lindsay",
-        "last_name": "Ferguson",
-        "avatar": "https://reqres.in/img/faces/8-image.jpg"
-        },
-        {
-        "id": 9,
-        "email": "tobias.funke@reqres.in",
-        "first_name": "Tobias",
-        "last_name": "Funke",
-        "avatar": "https://reqres.in/img/faces/9-image.jpg"
-        },]
+
+    const dispatch = useDispatch()
+
+    const users = []
+
+    useEffect(() => {
+        dispatch(fetchAllUsers())
+    }, [dispatch])
+    
 
   return (
     <div className='container'>
--- src/store/slices/users/index.js
+++ src/store/slices/users/index.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
+//Axios
+import axios from "axios";
 
 export const userSlice = createSlice({
     name: 'users',
@@ -6,8 +8,22 @@ export const userSlice = createSlice({
         list: []
     },
     reducers:{
-
+        setUsersList: (state, action) =>{
+            state.list = action.payload
+        }
     }
 })
 
+export const { setUsersList } = userSlice.actions
+
 export default userSlice.reducer;
+
+export const fetchAllUsers = () =>{
+    return (dispatch) => {
+        axios.get('https://reqres.in/api/users?page=2')
+            .then((response)=>{
+                dispatch(setUsersList(response.data.data))
+            })
+            .catch((error)=> console.log(error))
+    }
+}
